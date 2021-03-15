deecd99bba3906072e57735120b1bedc596119c7
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-15 20:44:37 +0530

    Setup Firebase to work with React

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyB9jv7BKLAhobjU0MBEXS9cgoJVJcDD16E",
+	authDomain: "react-firegram-c597a.firebaseapp.com",
+	projectId: "react-firegram-c597a",
+	storageBucket: "react-firegram-c597a.appspot.com",
+	messagingSenderId: "158661631487",
+	appId: "1:158661631487:web:c3591bd686b0cd2d4a9bca",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Use this variable to interact with the Storage (Store Images)
+export const projectStorage = firebase.storage();
+
+// Use this variable to interact with the Firestore Database
+export const projectFirestore = firebase.firestore();
