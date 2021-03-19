569ae973ead49eda21c823c143845d9b6e125edb
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-19 18:48:58 +0530

    Added styles & creating a custom hook

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -21,7 +21,10 @@ const UploadForm = () => {
 
 	return (
 		<form>
-			<input type="file" onChange={changeHandler} />
+			<label>
+				<input type="file" onChange={changeHandler} />
+				<span>+</span>
+			</label>
 			<div className="output">
 				{error && <div className="error">{error}</div>}
 				{file && <div>{file.name}</div>}
