c3f86b8dcfccfa054d5c49bd7b97b73ae3a11d67
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-24 19:22:39 +0530

    Working on the Progress Bar

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
 const ProgressBar = ({ file, setFile }) => {
 	const { url, progress } = useStorage(file);
-	console.log(url, progress);
 
-	return <div className="progress-bar">Progress</div>;
+	useEffect(() => {
+		if (url) {
+			setFile(null);
+		}
+	}, [url, setFile]);
+
+	return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 
 export default ProgressBar;
