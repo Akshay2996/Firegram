eebc27f9f9cb747ebe356e33d7a78d966f7c824f
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-26 19:17:38 +0530

    Displaying Images stored in the firestore database

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,7 +1,20 @@
 import React from "react";
+import useFirestore from "../hooks/useFirestore";
 
 const ImageGrid = () => {
-	return <div className="image-grid">Images</div>;
+	const { docs } = useFirestore("images");
+	console.log(docs);
+
+	return (
+		<div className="image-grid">
+			{docs &&
+				docs.map((doc) => (
+					<div className="image-wrap" key={doc.id}>
+						<img src={doc.url} alt="uploaded pic" />>
+					</div>
+				))}
+		</div>
+	);
 };
 
 export default ImageGrid;
