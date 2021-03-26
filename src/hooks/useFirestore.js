ae74fbae63afa9d3036e445d2c1b2e08ae02dca0
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-26 17:06:34 +0530

    Added custom hook for displaying images

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -3,6 +3,22 @@ import { projectFirestore } from "../firebase/config";
 
 const useFirestore = (collection) => {
 	const [docs, setDocs] = useState([]);
+
+	useEffect(() => {
+		const unSub = projectFirestore
+			.collection(collection)
+			.orderBy("createdAt", "desc")
+			.onSnapshot((snap) => {
+				let documents = [];
+				snap.forEach((doc) => {
+					documents.push({ ...doc.data(), id: doc.id });
+				});
+				setDocs(documents);
+			});
+
+		// Cleanup function
+		return () => unSub();
+	}, [collection]);
 	return { docs };
 };
 
